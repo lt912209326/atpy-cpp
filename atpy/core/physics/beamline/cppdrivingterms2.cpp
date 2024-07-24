717db68dbaee87b2fc2014fded5984c64892a888
taoliu95 <159145134+taoliu95@users.noreply.github.com>
2024-07-24 10:51:56 +0800

    Add files via upload
    convert the encoding from UTF-8 to UTF-8 with BOM, error occurs if the file is saved as both UTF-8 and LF.

--- atpy/core/physics/beamline/cppdrivingterms2.cpp
+++ atpy/core/physics/beamline/cppdrivingterms2.cpp
@@ -1,4 +1,4 @@
-#ifndef _CPPDRIVINGTERMS2_CPP_
+﻿#ifndef _CPPDRIVINGTERMS2_CPP_
 #define _CPPDRIVINGTERMS2_CPP_
 
 
@@ -86,15 +86,22 @@ void computmdrivingTerms_nonperiod(vector<CppComponent*>& bline, vector<Multipol
     }
 
 
-    size_t comp_pos_index=-1, comp_pos=bline[mult_pos[0] ]->position, slice_pos=-1,nslice=bline[mult_pos[0] ]->elem->nslice, sext_slice_cnt=0 ;
-    for(size_t i=0;i<multipole_slice;i++)
+    int comp_pos_index=-1, comp_pos=bline[mult_pos[0] ]->position, slice_pos=-1,nslice=bline[mult_pos[0] ]->elem->nslice, sext_slice_cnt=0 ;
+    for(int i=0;i<multipole_slice;i++)
     {
+        
+        if(stat.printout )cout<<"0 slice_i : "<<i<<endl;
         if(!(slice_pos<nslice)|| comp_pos_index<0 ){
             // 切片计数大于等于元件切片数或开始时，组件元素更换
             comp_pos_index+=1;
+            if(stat.printout )cout<<"0.1 slice_i : "<<i<<endl;
+            if(stat.printout )cout<<"0.1 slice_i : "<<comp_pos_index<<endl;
+            if(stat.printout )cout<<"0.1 slice_i : "<<mult_pos[comp_pos_index ]<<endl;
             comp_pos = bline[mult_pos[comp_pos_index ] ]->position;
+            if(stat.printout )cout<<"0.2 slice_i : "<<i<<endl;
             nslice=bline[mult_pos[comp_pos_index ] ]->elem->nslice;
             slice_pos=0;
+            if(stat.printout )cout<<"0.3 slice_i : "<<i<<endl;
             if(stat.nonlineartermonly){
                 bline[comp_pos-1]->tws(-1,CHROMX)=dQx;
                 bline[comp_pos-1]->tws(-1,CHROMY)=dQy;
@@ -104,7 +111,9 @@ void computmdrivingTerms_nonperiod(vector<CppComponent*>& bline, vector<Multipol
             }
 
             a2L = b2L = b3L = b4L = 0;
+        if(stat.printout )cout<<"0.4 slice_i : "<<i<<endl;
             elem=bline[comp_pos]->elem;
+        if(stat.printout )cout<<"0.5 slice_i : "<<i<<endl;
             switch (elem->kind) 
             {
             case SEXTUPOLE:
@@ -123,11 +132,13 @@ void computmdrivingTerms_nonperiod(vector<CppComponent*>& bline, vector<Multipol
                 break;
             }      
         }
+        if(stat.printout )cout<<"1 slice_i : "<<i<<endl;
         if(SEXTUPOLE==elem->kind){
             sext_slice_index[sext_slice_cnt]=i;
             sext_slice_cnt+=1;
         }
         
+        if(stat.printout )cout<<"2 slice_i : "<<i<<endl;
         if (a2L || b2L || b3L || b4L) {
             if (i>0 && slice_pos>0) {
                 betax1 = (bline[comp_pos]->tws(slice_pos,BETAX)  + bline[comp_pos]->tws(slice_pos-1,BETAX) )/2;
@@ -233,6 +244,7 @@ void computmdrivingTerms_nonperiod(vector<CppComponent*>& bline, vector<Multipol
             md[i].b2L = b2L;
             md[i].b3L = b3L;
         }
+        if(stat.printout )cout<<"3 slice_i : "<<i<<endl;
         // LOC[i][LH11001]=h11001.real();
         // LOC[i][LH00111]=h00111.real();
         // LOC[i][LH11001]=LOC[i-1][LH11001]+LOC[i][CHROMX];
@@ -250,6 +262,7 @@ void computmdrivingTerms_nonperiod(vector<CppComponent*>& bline, vector<Multipol
             // elem = elem->succ;
         slice_pos+=1;
     }
+    if(stat.printout )cout<<"4 slice_i : "<<i<<endl;
 
     /*  Doi with the leading-order quad and sext terms */
     GLB[H11001] = h11001/PI;
@@ -279,6 +292,8 @@ void computmdrivingTerms_nonperiod(vector<CppComponent*>& bline, vector<Multipol
         if(  (stat.combineddipole && DIPOLE==elem_kind) || QUADRUPOLE==elem_kind || SEXTUPOLE==elem_kind || OCTUPOLE==elem_kind) continue;
         memcpy(&bline[i]->tws(-1,LH11001),&bline[i-1]->tws(-1,LH11001), num_local_RDTs*__SIZEOF_DOUBLE__ );
     }
+    
+    if(stat.printout )cout<<"5 slice_i : "<<i<<endl;
 
     if(!(stat.leaderordertermonly)) {
         /*  compute sextupole contributions to second-order terms */
@@ -414,6 +429,7 @@ void computmdrivingTerms_nonperiod(vector<CppComponent*>& bline, vector<Multipol
     GLB[DNUY_DJY]=0.5*dnuy_dJy*nPeriods;
     }
 
+    if(stat.printout )cout<<"computmdrivingTerms_nonperiod finished"<<endl;
 }
 
 #endif
